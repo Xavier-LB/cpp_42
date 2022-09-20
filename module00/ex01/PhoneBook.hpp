8fe0c09cb8125bceb94f162a3c03bb55b7148890
XavierLB <xavier.lebaux@gmail.com>
2022-09-20 18:52:26 +0200

    .

--- module00/ex01/PhoneBook.hpp
+++ module00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: xle-baux <xle-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 14:53:23 by xle-baux          #+#    #+#             */
-/*   Updated: 2022/09/18 17:27:01 by xle-baux         ###   ########.fr       */
+/*   Updated: 2022/09/20 15:08:52 by xle-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,39 @@
 class	Contact
 {
 private:
+
 	std::string	m_first_name;
 	std::string	m_last_name;
 	std::string	m_nickname;
 	std::string	m_phone_number;
 	std::string	m_secret;
 
+
 public:
+
 	void		fillInfo(std::string tmp[5]);
 	std::string	getFirstName();
 	std::string	getLastName();
 	std::string	getNickname();
 	std::string	getPhoneNumber();
 	std::string	getSecret();
+	
 };
 
 class	PhoneBook
 {
+
 private:
+
 	Contact contact[8];
 
+
 public:
+
 	void	addContact(int i);
 	void	printContactList();
 	void	printContactInfo();
+
 };
 
 #endif
--- module00/ex02/Account.cpp
+++ module00/ex02/Account.cpp
@@ -6,9 +6,86 @@
 /*   By: xle-baux <xle-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 17:51:22 by xle-baux          #+#    #+#             */
-/*   Updated: 2022/09/18 18:01:21 by xle-baux         ###   ########.fr       */
+/*   Updated: 2022/09/20 18:51:58 by xle-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
 
+int	Account::_nbAccounts = 0;
+int	Account::_totalAmount = 0;
+int	Account::_totalNbDeposits = 0;
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account( int initial_deposit ) {
+	_accountIndex = ++_nbAccounts;
+	_amount = initial_deposit;
+	_totalAmount += initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	std::cout << "index:" << _accountIndex;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";created" << std::endl;
+}
+
+Account::Account( void ) {
+	_accountIndex = _nbAccounts;
+	_amount = 0;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	std::cout << "index:" << _accountIndex;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";created" << std::endl;
+}
+
+Account::~Account( void ) {
+	_accountIndex = _nbAccounts;
+	std::cout << "index:" << _accountIndex;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";closed" << std::endl;
+}
+
+int	Account::getNbAccounts( void ) {
+	return (Account::_nbAccounts);
+}
+
+int	Account::getTotalAmount( void ) {
+	return (Account::_totalAmount);
+}
+
+int	Account::getNbDeposits( void ) {
+	return (Account::_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void ) {
+	return (Account::_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void ) {
+	std::cout << "accounts:" << getNbAccounts();
+	std::cout << ";total:" << getTotalAmount();
+	std::cout << ";deposits:" << getNbDeposits();
+	std::cout << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+
+// void	makeDeposit( int deposit ) {
+
+// }
+
+// bool	makeWithdrawal( int withdrawal ) {
+
+// }
+
+// int		checkAmount( void ) {
+
+// }
+
+void	Account::displayStatus( void ) const {
+	std::cout << "index:" << &_accountIndex;
+	std::cout << ";amount:" << &_amount;
+	std::cout << ";deposits:" << &_nbDeposits;
+	std::cout << ";withdrawals:" << &_nbWithdrawals << std::endl;
+
+}
