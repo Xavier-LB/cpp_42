462c3610f89818f38b6a73e9b51e07ab54721a6d
XavierLB <xavier.lebaux@gmail.com>
2022-12-04 18:49:57 +0100

    .

--- module05/ex03/Intern.cpp
+++ module05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: xle-baux <xle-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 15:42:52 by xle-baux          #+#    #+#             */
-/*   Updated: 2022/12/02 16:04:13 by xle-baux         ###   ########.fr       */
+/*   Updated: 2022/12/04 18:49:32 by xle-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,25 +36,115 @@ Intern::~Intern()
 	std::cout << "(Form) Destructor" << std::endl;
 }
 
-Form * Intern::makeForm(std::string formName, std::string target)
-{
-    std::string formNames[] = {
-        "robotomy request",
-        "presidential pardon",
-        "shrubbery creation"
-    };
-    Form*    forms[] = {
-        new RobotomyRequestForm( target ),
-        new PresidentialPardonForm( target ),
-        new ShrubberyCreationForm( target )
-    };
+const std::string    Intern::_formName[] = {"shrubbery creation", "robotmy request",
+                                                    "presidential pardon"};
+Intern::functions[] = { &Intern::createShrubberyCreationForm, 
+                                                        &Intern::createRobotomyRequestForm,
+                                                        &Intern::createPresidentialPardonForm };
+
+// Form * Intern::makeForm(std::string formName, std::string target)
+// {
+//     std::string formNames[] = {
+//         "robotomy request",
+//         "presidential pardon",
+//         "shrubbery creation"
+//     };
+//     Form*    forms[] = {
+//         new RobotomyRequestForm( target ),
+//         new PresidentialPardonForm( target ),
+//         new ShrubberyCreationForm( target )
+//     };
     
-    for ( int i(0); i < 3; i++ ) {
-        if ( formName == formNames[i] ) {
-            std::cout << "Intern creates " << formName << std::endl;
-            return forms[i];
-        }
-    }
-    std::cout << "Intern cannot create " << formName << " form" << std::endl;
-    return 0;
+//     for ( int i(0); i < 3; i++ ) {
+//         if ( formName == formNames[i] ) {
+//             std::cout << "Intern creates " << formName << std::endl;
+//             return forms[i];
+//         }
+//     }
+//     std::cout << "Intern cannot create " << formName << " form" << std::endl;
+//     return 0;
+// }
+
+Form* Intern::makeForm(std::string const & formName, std::string const & target) {
+    std::string    target(target);
+
+    if (target.empty() || target.size() == 0)
+        target = "Unknow";
+
+    for (int i = 0; i < 3; i++)
+        if (formName.compare(this->_formName[i]) == 0)
+            return (this->*_functions[i])(target);
+    
+    throw Intern::UnknowFormException();
 }
+
+const char*    Intern::UnknowFormException::what() const throw() {
+    return ("Intern: Can't find the requested form!");
+}
+
+Form*    Intern::createShrubberyCreationForm(std::string const target) const {
+    return new ShrubberyCreationForm(target);
+}
+
+Form*    Intern::createRobotomyRequestForm(std::string const target) const {
+    return new RobotomyRequestForm(target);
+}
+
+Form*    Intern::createPresidentialPardonForm(std::string const target) const {
+    return new PresidentialPardonForm(target);
+}
+
+
+// #include <iostream>
+// #include "Intern.hpp"
+// #include "ShrubberyCreationForm.hpp"
+// #include "RobotomyRequestForm.hpp"
+// #include "PresidentialPardonForm.hpp"
+
+// const std::string    Intern::_formName[FORM_NUMBERS] = {"shrubbery creation", "robotmy request",
+//                                                     "presidential pardon"};
+// const functions        Intern::_functions[FORM_NUMBERS] = { &Intern::createShrubberyCreationForm, 
+//                                                         &Intern::createRobotomyRequestForm,
+//                                                         &Intern::createPresidentialPardonForm };
+
+// Intern::Intern() {}
+
+// Intern::Intern(Intern const & ref) {
+//     (void)ref;
+// }
+
+// Intern::~Intern() {}
+
+// Intern&    Intern::operator=(Intern const & ref) {
+//     (void)ref;
+//     return *this;
+// }
+
+// AForm*    Intern::makeForm(std::string const & formName, std::string const & formTarget) {
+//     std::string    target(formTarget);
+
+//     if (target.empty() || target.size() == 0)
+//         target = "Unknow";
+
+//     for (int i = 0; i < FORM_NUMBERS; i++)
+//         if (formName.compare(this->_formName[i]) == 0)
+//             return (this->*_functions[i])(target);
+    
+//     throw Intern::UnknowFormException();
+// }
+
+// const char*    Intern::UnknowFormException::what() const throw() {
+//     return ("Intern: Can't find the requested form!");
+// }
+
+// Form*    Intern::createShrubberyCreationForm(std::string const target) const {
+//     return new ShrubberyCreationForm(target);
+// }
+
+// Form*    Intern::createRobotomyRequestForm(std::string const target) const {
+//     return new RobotomyRequestForm(target);
+// }
+
+// Form*    Intern::createPresidentialPardonForm(std::string const target) const {
+//     return new PresidentialPardonForm(target);
+// }
--- module05/ex03/Intern.hpp
+++ module05/ex03/Intern.hpp
@@ -6,7 +6,7 @@
 /*   By: xle-baux <xle-baux@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 15:42:49 by xle-baux          #+#    #+#             */
-/*   Updated: 2022/12/02 15:55:05 by xle-baux         ###   ########.fr       */
+/*   Updated: 2022/12/04 18:49:26 by xle-baux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,26 @@
 class Intern
 {
 private:
-	
+	const std::string *_formName;
+
 public:
 	Intern();
 	Intern(Intern const & src);
 	Intern & operator=(Intern const & src);
 	virtual ~Intern();
 
-	Form * makeForm(std::string formName, std::string target);
+
+	// const functions _functions;
+	typedef int (*functions)(std::string target);
+	Form* makeForm(std::string const & formName, std::string const & target);
+	Form* createShrubberyCreationForm(std::string const target) const;
+	Form* createRobotomyRequestForm(std::string const target) const;
+	Form* createPresidentialPardonForm(std::string const target) const;
+
+	class UnknowFormException : public std::exception
+	{
+		const char *what() const throw();
+	};
 };
 
 #endif
