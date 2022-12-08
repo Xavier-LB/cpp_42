56b32a0f26d1a96979935f215f5c88591dfdad37
XavierLB <xavier.lebaux@gmail.com>
2022-12-08 21:28:57 +0100

    .

--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -3,31 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xle-baux <xle-baux@student.42.fr>          +#+  +:+       +#+        */
+/*   By: xlb <xlb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 16:05:04 by xle-baux          #+#    #+#             */
-/*   Updated: 2022/12/08 18:02:20 by xle-baux         ###   ########.fr       */
+/*   Updated: 2022/12/08 21:23:33 by xlb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
-#include <iostream>
-#include <cstdlib>
-#include <unistd.h>
-#include <ctime>
 
+void ft_tolower(char &arr)
+{
+	arr = std::tolower(static_cast<unsigned char>(arr));
+}
+
+void ft_toupper(char &arr)
+{
+	arr = std::toupper(static_cast<unsigned char>(arr));
+}
 
 int main()
 {
-	
+	char a[] = {'A', 'B', 'C'};
+
+	std::cout << "Original:" <<
+	"\n\ta[0]: " << a[0] <<
+	"\n\ta[1]: " << a[1] <<
+	"\n\ta[2]: " << a[2] <<
+	std::endl << std::endl;
+
+	iter(a, sizeof(a) / sizeof(char), ft_tolower);
+
+	std::cout << "Changed:" <<
+	"\n\ta[0]: " << a[0] <<
+	"\n\ta[1]: " << a[1] <<
+	"\n\ta[2]: " << a[2] <<
+	std::endl << std::endl;
+
+	iter(a, 2, ft_toupper);
+
+	std::cout << "Changed:" <<
+	"\n\ta[0]: " << a[0] <<
+	"\n\ta[1]: " << a[1] <<
+	"\n\ta[2]: " << a[2] <<
+	std::endl << std::endl;
 	return 0;
 }
 
 template <typename T>
-void iter(T & array, int len, void (*function)(T))
+void iter(T * array, size_t len, void (*function)(T &))
 {
-	for (int i(0); i < len; i++)
+	for (size_t i = 0; i < len; i++)
 	{
-		function()(array[i]);
+		function(array[i]);
 	}
 }
--- module07/ex02/Array.hpp
+++ module07/ex02/Array.hpp
@@ -1,21 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xlb <xlb@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/08 16:05:01 by xle-baux          #+#    #+#             */
-/*   Updated: 2022/12/08 21:22:12 by xlb              ###   ########.fr       */
+/*   Created: 2022/12/08 21:26:22 by xlb               #+#    #+#             */
+/*   Updated: 2022/12/08 21:28:23 by xlb              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ITER_HPP
-# define ITER_HPP
-
-#include <iostream>
-
-template <typename T>
-void iter(T * array, size_t len, void (*function)(T &));
-
-#endif
+template <class T>
+class Array
+{
+private:
+public:
+	Array() {}
+	~Array() {}
+};
